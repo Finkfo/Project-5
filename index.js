b603042dd231e273e0899c0edc17c452dfd681bd
Finkfo <valentin.valleepro@gmail.com>
2022-12-05 18:50:21 +0100

    git ignore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4444;
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Hello World! ou Salut les terriens! <3");
 });
 
 app.listen(port, function () {
