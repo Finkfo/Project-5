39f90a3446fd1a7b8dbc78a7c64a5703f4cd4203
Finkfo <valentin.valleepro@gmail.com>
2022-12-06 20:00:12 +0100

    MARDI 20H

--- index.js
+++ index.js
@@ -65,14 +65,58 @@ app.delete('/pokemon/delete', jsonParser, (req, res) => {
   });
 });
 
-app.updateOne('/pokemon/insert', jsonParser, (req, res) => {
-  const oldvalues = req.body.oldvalues;
+app.post('/pokemon/updateOne', jsonParser, (req, res) => {
   const body = req.body;
-  const filter = { oldvalues };
+  const oldvalues = req.body.oldvalues;
+  const filter = { name:oldvalues };
+  const newvalues = {
+    $set: {
+      name: req.body.newvalues
+    }
+  };
   console.log('Got body:', body);
   const dbConnect = dbo.getDb();
   dbConnect
   .collection("pokemon")
+  .updateOne(filter, newvalues)
+  .then(function (result, err) {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+      res.json(result);
+  });
+}); 
+
+
+app.get("/pokedex/list", function (req, res) {
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+  //premier test permettant de récupérer mes pokedex !
+  dbConnect
+    .collection("pokedex")
+    .find({}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching pokedex!");
+      } else {
+        res.json(result);
+      }
+    });
+    /*
+    Bref lisez la doc, 
+    il y a plein de manières de faire ce qu'on veut :) 
+    */
+    
+});
+
+app.post('/pokedex/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("pokedex")
+  .insertOne(body)
   .then(function (result, err) {
     if (err) {
       res.status(400).send(err.message);
@@ -80,3 +124,115 @@ app.updateOne('/pokemon/insert', jsonParser, (req, res) => {
       res.json(result);
   });
 });
+
+app.delete('/pokedex/delete', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("pokedex")
+  .deleteOne(body)
+  .then(function (result, err) {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+      res.json(result);
+  });
+});
+
+app.post('/pokedex/updateOne', jsonParser, (req, res) => {
+  const body = req.body;
+  const oldvalues = req.body.oldvalues;
+  const filter = { name:oldvalues };
+  const newvalues = {
+    $set: {
+      name: req.body.newvalues
+    }
+  };
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("pokedex")
+  .updateOne(filter, newvalues)
+  .then(function (result, err) {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+      res.json(result);
+  });
+}); 
+
+
+app.get("/types/list", function (req, res) {
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+  //premier test permettant de récupérer mes types !
+  dbConnect
+    .collection("types")
+    .find({}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching types!");
+      } else {
+        res.json(result);
+      }
+    });
+    /*
+    Bref lisez la doc, 
+    il y a plein de manières de faire ce qu'on veut :) 
+    */
+    
+});
+
+app.post('/types/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("types")
+  .insertOne(body)
+  .then(function (result, err) {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+      res.json(result);
+  });
+});
+
+app.delete('/types/delete', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("types")
+  .deleteOne(body)
+  .then(function (result, err) {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+      res.json(result);
+  });
+});
+
+app.post('/types/updateOne', jsonParser, (req, res) => {
+  const body = req.body;
+  const oldvalues = req.body.oldvalues;
+  const filter = { name:oldvalues };
+  const newvalues = {
+    $set: {
+      name: req.body.newvalues
+    }
+  };
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("types")
+  .updateOne(filter, newvalues)
+  .then(function (result, err) {
+    if (err) {
+      res.status(400).send(err.message);
+    }
+      res.json(result);
+  });
+}); 
