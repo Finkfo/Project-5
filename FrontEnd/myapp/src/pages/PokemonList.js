9280f28ff5a7594a7dd59b99093c5a5816ff0782
Finkfo <valentin.valleepro@gmail.com>
2022-12-11 18:45:39 +0100

    Dimanche 19h Pokemonlist update

--- FrontEnd/myapp/src/pages/PokemonList.js
+++ FrontEnd/myapp/src/pages/PokemonList.js
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
+import { getAll } from "../api/pokemon";
 import Menu from "../components/menu";
-
 export function Pokemon(props) {
-    return <div>
-        <Menu />
-    </div>
+    const [pokemons, setPokemons] = useState([]);
+
+    //va s'executer seulement au lancement du composant (dep: [])
+    useEffect(() => {
+      // récupérer la liste des users seulement au chargement du composant !
+      const pokemonsFetched = getAll();
+      pokemonsFetched
+        .then((result) => setPokemons(result))
+        .catch((error) =>
+          console.error("Erreur avec notre API :", error.message)
+        );
+    }, []);
+  
+    return (<div>
+      <Menu />
+      <div className="pokemon-list">
+        <div class="flex">
+          {pokemons.map((pokemon, key) => {
+            return (
+              <div key={key} className="bloc-pokemon">
+                <img className="avatar" src={pokemon.img} />
+                <h2>Nom: {pokemon.name} | Type: </h2> 
+                <img className="types" src={pokemon.type} />
+                {/* <button onClick={()=>addToPokedex(pokemon._id)}>Capturer !</button> */}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      </div>
+    );
 
 
 }
